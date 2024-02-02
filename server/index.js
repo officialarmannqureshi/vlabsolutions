001a5f0fa3e7c32b4ba9accfad114610cc5a8df3
Restyled.io <commits@restyled.io>
2024-02-02 21:51:07 +0000

    Restyled by clang-format

--- server/index.js
+++ server/index.js
@@ -1,45 +1,39 @@
-import express from "express";
 import colors from "colors";
+import cors from "cors";
 import dotenv from "dotenv";
+import express from "express";
+import createProxyMiddleware from "http-proxy-middleware";
 import morgan from "morgan";
+
 import connectDB from "./src/config/db.js";
 import authRoutes from "./src/routers/authRoute.js";
-import cors from "cors";
-import createProxyMiddleware from "http-proxy-middleware";
 
-
-//configure env
+// configure env
 dotenv.config();
 
-//databse config
+// databse config
 connectDB();
 
-//rest object
+// rest object
 const app = express();
 
 app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT"],  // Change the semicolon to a comma here
+  origin : "*",
+  methods : [ "GET", "POST", "PUT" ], // Change the semicolon to a comma here
 }));
 
 app.use(express.json());
 app.use(morgan("dev"));
 
-
-//routes
+// routes
 app.use("/api/v1/auth", authRoutes);
-app.use("/",(req,res)=>{
-  res.send(`<h1>Welcome to the Auth Server</h1>`);
-})
+app.use("/", (req, res) => { res.send(`<h1>Welcome to the Auth Server</h1>`); })
 
-
-//PORT
+// PORT
 const PORT = process.env.PORT || 8080;
 
-//run listen
+// run listen
 app.listen(PORT, () => {
-  console.log(
-    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
-      .white
-  );
+  console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`
+                  .bgCyan.white);
 });
