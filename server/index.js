c5d839802ae88cb78d8f0ec04f53e5ae1f11f607
Restyled.io <commits@restyled.io>
2024-02-02 21:51:09 +0000

    Restyled by prettier

--- server/index.js
+++ server/index.js
@@ -17,23 +17,29 @@ connectDB();
 // rest object
 const app = express();
 
-app.use(cors({
-  origin : "*",
-  methods : [ "GET", "POST", "PUT" ], // Change the semicolon to a comma here
-}));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT"], // Change the semicolon to a comma here
+  }),
+);
 
 app.use(express.json());
 app.use(morgan("dev"));
 
 // routes
 app.use("/api/v1/auth", authRoutes);
-app.use("/", (req, res) => { res.send(`<h1>Welcome to the Auth Server</h1>`); })
+app.use("/", (req, res) => {
+  res.send(`<h1>Welcome to the Auth Server</h1>`);
+});
 
 // PORT
 const PORT = process.env.PORT || 8080;
 
 // run listen
 app.listen(PORT, () => {
-  console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`
-                  .bgCyan.white);
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white,
+  );
 });
