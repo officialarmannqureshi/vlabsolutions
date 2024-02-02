3e58deadae79798318479f7cf59b7ad75d9c8287
Restyled.io <commits@restyled.io>
2024-02-02 21:11:09 +0000

    Restyled by clang-format

--- server/index.js
+++ server/index.js
@@ -1,46 +1,41 @@
-import express from "express";
 import colors from "colors";
+import cors from "cors";
 import dotenv from "dotenv";
+import express from "express";
 import morgan from "morgan";
+
 import connectDB from "./src/config/db.js";
 import authRoutes from "./src/routers/authRoute.js";
-import cors from "cors";
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
-//configure env
+// configure env
 dotenv.config();
 
-//databse config
+// databse config
 connectDB();
 
-//rest object
+// rest object
 const app = express();
 
 app.use('/v1/storage', createProxyMiddleware({
-  target: 'https://cloud.appwrite.io',
-  changeOrigin: true,
-}));
-//middelwares
+          target : 'https://cloud.appwrite.io',
+          changeOrigin : true,
+        }));
+// middelwares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-
-//routes
+// routes
 app.use("/api/v1/auth", authRoutes);
-app.use("/",(req,res)=>{
-  res.send(`<h1>Welcome to the Auth Server</h1>`);
-})
+app.use("/", (req, res) => { res.send(`<h1>Welcome to the Auth Server</h1>`); })
 
-
-//PORT
+// PORT
 const PORT = process.env.PORT || 8080;
 
-//run listen
+// run listen
 app.listen(PORT, () => {
-  console.log(
-    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
-      .white
-  );
+  console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`
+                  .bgCyan.white);
 });
